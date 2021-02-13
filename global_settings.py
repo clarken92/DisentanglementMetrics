3ed484ba1289cf97b72f63cd037897e1caa05500
Kien Do <hunken92@gmail.com>
2021-02-13 12:51:14 +1100

    Update global_settings

--- global_settings.py
+++ global_settings.py
@@ -1,13 +1,9 @@
-USER = "dkie"
-# PYTHON_EXE = f"/home/{USER}/InstalledSoftwares/anaconda3/envs/Tensorflow/bin/python"
-PYTHON_EXE = f"/home/{USER}/InstalledSoftwares/anaconda3/envs/PommerMan/bin/python"
+USER = "username"
+PYTHON_EXE = f"/home/{USER}/InstalledSoftwares/anaconda3/envs/Tensorflow/bin/python"
 
 PROJECT_NAME = "DisentanglementMetrics"
-# PROJECT_DIR = f"/home/{USER}/Working/Workspace/Github/{PROJECT_NAME}"
-# RESULTS_DIR = f"/home/{USER}/Working/Results/Github/{PROJECT_NAME}"
-
-PROJECT_DIR = f"/home/{USER}/Working/Workspace/Github/DeepLearningProjects/{PROJECT_NAME}"
-RESULTS_DIR = f"/home/{USER}/Working/Results/Github/DeepLearningProjects/{PROJECT_NAME}"
+PROJECT_DIR = f"/home/{USER}/Working/Workspace/Github/{PROJECT_NAME}"
+RESULTS_DIR = f"/home/{USER}/Working/Results/Github/{PROJECT_NAME}"
 
 DATA_DIR = f"/home/{USER}/Working/Data/"
 RAW_DATA_DIR = DATA_DIR + "Raw"
